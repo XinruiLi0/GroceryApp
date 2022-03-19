6e8990fbcaa719f330066ba6959ed1258db87efd
XinruiLi0 <xinruili3@cmail.carleton.ca>
2022-03-19 16:46:00 -0400

    Adding jtds and database credential

--- GroceryAPP2/app/src/main/java/com/example/groceryapp/MainActivity.java
+++ GroceryAPP2/app/src/main/java/com/example/groceryapp/MainActivity.java
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +28,36 @@ public class MainActivity extends AppCompatActivity {
                 // To selection menu
                 Intent intent = new Intent(MainActivity.this,Register.class);
                 startActivity(intent);
+
+            }
+        });
+
+        Button test = (Button) findViewById(R.id.test);
+        test.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                DBUtil db = new DBUtil();
+                Connection conn = db.conclass();
+                if (conn != null) {
+                    try {
+                        ArrayList<ArrayList<String>> result = new ArrayList<>();
+                        String sql = "select * from test";
+                        Statement smt = conn.createStatement();
+                        ResultSet rs = smt.executeQuery(sql);
+                        ResultSetMetaData rsm =rs.getMetaData();
+                        int col = rsm.getColumnCount();
+                        while (rs.next()) {
+                            ArrayList<String> row = new ArrayList<String>();
+                            for (int i = 1; i <= col; i++) {
+                                row.add(rs.getString(i));
+                            }
+                            result.add(row);
+                        }
+                        test.setText(result.toString());
+                        conn.close();
+                    } catch (Exception e) {
+                        Log.e("Error :", e.getMessage());
+                    }
+                }
 
             }
         });
