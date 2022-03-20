53e57ab3d6d2809d0cdf301d4ed8703ca73d9b84
XinruiLi0 <xinruili3@cmail.carleton.ca>
2022-03-19 21:15:20 -0400

    Recover database connection

--- GroceryAPP2/app/src/main/java/com/example/groceryapp/MainActivity.java
+++ GroceryAPP2/app/src/main/java/com/example/groceryapp/MainActivity.java
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +39,36 @@ public class MainActivity extends AppCompatActivity {
                 // To selection menu
                 Intent intent = new Intent(MainActivity.this,storeRegister.class);
                 startActivity(intent);
+
+            }
+        });
+
+        Button test = (Button) findViewById(R.id.test);
+        test.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                SQLConnection db = new SQLConnection();
+                Connection conn = db.conclass();
+                if (conn != null) {
+                    try {
+                        ArrayList<ArrayList<String>> result = new ArrayList<>();
+                        String sql = "select * from test";
+                        Statement smt = conn.createStatement();
+                        ResultSet rs = smt.executeQuery(sql);
+                        ResultSetMetaData rsm =rs.getMetaData();
+                        int col = rsm.getColumnCount();
+                        while (rs.next()) {
+                            ArrayList<String> row = new ArrayList<String>();
+                            for (int i = 1; i <= col; i++) {
+                                row.add(rs.getString(i));
+                            }
+                            result.add(row);
+                        }
+                        test.setText(result.toString());
+                        conn.close();
+                    } catch (Exception e) {
+                        Log.e("Error :", e.getMessage());
+                    }
+                }
 
             }
         });
---
+++ GroceryAPP2/app/src/main/java/com/example/groceryapp/SQLConnection.java
@@ -0,0 +1,31 @@
+package com.example.groceryapp;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SQLConnection {
+    private static String IP = "database-1.clc4hmfd10vk.us-east-1.rds.amazonaws.com";
+    private static String DBName = "grocery";
+    private static String USER = "admin";
+    private static String PWD = "groceryapp";
+
+    Connection con;
+
+    @SuppressLint("NewApi")
+    public Connection conclass() {
+        StrictMode.ThreadPolicy a = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(a);
+        String connectURL = null;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:jtds:sqlserver://" + IP + ":1433/" + DBName, USER, PWD);
+        } catch (Exception e) {
+            Log.e("Error :", e.getMessage());
+        }
+        return con;
+    }
+}
