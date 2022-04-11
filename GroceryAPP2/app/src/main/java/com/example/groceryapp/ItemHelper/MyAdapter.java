b5f816ed39eee896fd3873f49c831493d6a4d7fd
XinruiLi0 <xinruili3@cmail.carleton.ca>
2022-04-10 21:15:49 -0400

    Add restock time to category.

--- GroceryAPP2/app/src/main/java/com/example/groceryapp/ItemHelper/MyAdapter.java
+++ GroceryAPP2/app/src/main/java/com/example/groceryapp/ItemHelper/MyAdapter.java
@@ -3,6 +3,7 @@ package com.example.groceryapp.ItemHelper;
 import static android.widget.Toast.LENGTH_LONG;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -47,6 +48,11 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         holder.itemId = itemHelperClass.getItemId();
         holder.itemStock = itemHelperClass.getItemStock();
         holder.restockTime = itemHelperClass.getRestockTime();
+        if (Double.parseDouble(holder.itemStock) < 1) {
+            holder.itemQuantities.setText("Out of Stock");
+            holder.itemQuantities.setTextColor(Color.RED);
+            holder.itemNumber.setFocusable(false);
+        }
         holder.itemPrice.setText("$ " +itemHelperClass.getItemPrice());
         holder.itemCategory = itemHelperClass.getItemCategory();
         ArrayList<Integer> itemList = new ArrayList<Integer> ();
@@ -72,6 +78,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         TextView itemPrice;
         String itemCategory;
         ImageView itemImage;
+        TextView itemQuantities;
         EditText itemNumber;
 
         public MyViewHolder(@NonNull View itemView) {
@@ -80,13 +87,18 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             itemName = itemView.findViewById(R.id.itemName);
             itemPrice = itemView.findViewById(R.id.itemPrice);
             itemImage = itemView.findViewById(R.id.itemImage);
-
+            itemQuantities = itemView.findViewById(R.id.quantities);
             itemNumber = itemView.findViewById(R.id.number);
 
+            itemQuantities.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(view.getContext(), "The next restock time for "+itemName.getText()+" is "+restockTime, LENGTH_LONG).show();
+                }
+            });
+
             itemNumber.addTextChangedListener(new TextWatcher() {
-
                 public void afterTextChanged(Editable s) {
-
                     if (s.toString().isEmpty()) { return; }
                     int temp = Integer.parseInt(s.toString());
                     if (temp > Double.parseDouble(itemStock)) {
@@ -96,7 +108,6 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
                 }
 
                 public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
                 public void onTextChanged(CharSequence s, int start, int before, int count) {}
             });
         }
